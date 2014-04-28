a7a0ab642c5ae853c11fa4c0a54d5990e29322cf
jacksonfraga <jackson@jacksonfraga.com.br>
2014-04-27 22:15:06 -0300

    comentario codigo

--- SimuladorEscalonamento.Core/AlgoritmoFIFO.cs
+++ SimuladorEscalonamento.Core/AlgoritmoFIFO.cs
@@ -10,13 +10,16 @@ namespace SimuladorEscalonamento.Core
     {
         public override void ProximoTempo()
         {
+            // aqui empilha processos na fila quando houver novos no tempo atual, tras o PID do próximo da fila (ordem por entrada na fila)
             int pid = EmpilharFila();
 
             if (PIDAtual == 0)
             {
+                // pega apenas da fila caso haja um na espera e se não houver em execução
                 if (FilaEspera.Count > 0)
                 {
                     PIDAtual = pid;
+                    // retira da fila
                     RetirarFilaEspera(PIDAtual);
                 }
             }
